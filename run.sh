96be2ffc8319e46f57575f96cb5ae49f6b81fde9
Ruslan Dudka <ruslandudka2@gmail.com>
2021-10-13 16:01:32 +0300

    update env

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ read -p "Enter project name: " PROJECT_NAME
 PROJECT_NAME_REPLACE=$PROJECT_NAME
 PROJECT_ROOT_REPLACE=/var/www/$PROJECT_NAME-backend
 VIRTUALENV_HOME_REPLACE=/root/.virtualenvs/$PROJECT_NAME\_env
-PROJECT_SECRET_KEY=`tr -dc 'A-Za-z0-9!#$%&<>?@' </dev/urandom | head -c 64`
+PROJECT_SECRET_KEY=`tr -dc 'A-Za-z0-9!#%&<>?@' </dev/urandom | head -c 64`
 
 grep -rl "PROJECT_NAME_REPLACE" ../project | xargs sed -i 's+PROJECT_NAME_REPLACE+'$PROJECT_NAME_REPLACE'+g'
 grep -rl "PROJECT_ROOT_REPLACE" ../project | xargs sed -i 's+PROJECT_ROOT_REPLACE+'$PROJECT_ROOT_REPLACE'+g'
